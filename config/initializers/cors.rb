2683a1083138c455bf2bf471f7ba795f2d79dcca
Stephanie Chamblee <stephaniejoychamblee@gmail.com>
2025-05-03 11:58:57 -0400

    Create Job Applications controller with CRUD methods

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:5173"
-
-    resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+    resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      get "hello", to: "hello#index"
-    end
-  end
+  resources :job_applications
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
